0d25b3efb023dbeac352eac11fef37f6dae51211
larrychen5155210 <z5155210@gmail.com>
2022-09-17 03:46:04 +0800

    update_3

--- password.py
+++ password.py
@@ -2,7 +2,7 @@
 password = 'a123456'
 i = 3
 user = input('請輸入你的密碼: ')
-while True:
+while i > 0:
 	i = i - 1
 	if user == password:
 		print('登入成功')
@@ -10,7 +10,7 @@ while True:
 	else:
 		if i > 0:
 			print('密碼錯誤!! 還有', i, '次機會')
+			user = input('請重新輸入你的密碼: ')
 		else:
 			print('登入失敗 請重新嘗試')
-			break
-		user = input('請重新輸入你的密碼: ')
+	
